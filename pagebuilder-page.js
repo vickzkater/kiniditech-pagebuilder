85e8c170eebb136e66580315041c50687308ce5b
vickzkater <vickzkater@gmail.com>
2020-06-17 08:21:31 +0000

    Add Page Element: Video

--- pagebuilder-page.js
+++ pagebuilder-page.js
@@ -14,9 +14,9 @@
  *
  * Support Form Elements:
  * - Masthead (Support Multiple Item)
- * - Text (Support Multiple Item)
+ * - Text (Support 3 Items)
  * - Image (Support Multiple Item)
- * - Image + Text + Button (Support Multiple Item)
+ * - Image + Text + Button (Support 3 Items)
  * - Video
  * - Button
  */
@@ -141,6 +141,10 @@ function add_content_element_page(
         case "image + text + button":
             html_content_element = set_element_page_imagetextbutton(collapsed, uniqid, data);
             break;
+
+        case "video":
+            html_content_element = set_element_page_video(collapsed, uniqid, data);
+            break;
   
         default:
             alert("NO CONTENT ELEMENT TYPE SELECTED");
@@ -165,6 +169,10 @@ function add_content_element_page(
             break;
 
         case "image + text + button":
+            initialize_tinymce('.page-element-text-editor');
+            break;
+
+        case "video":
             initialize_tinymce('.page-element-text-editor');
             break;
     }
@@ -774,16 +782,15 @@ function set_element_page_imagetextbutton(collapsed, uniqid, data) {
                     html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Total Items <span class="required">*</span></label>';
                     html += '<div class="col-md-6 col-sm-6 col-xs-12">';
                         html += '<select name="v_page_element_total_item[' + section_page_id + '][' + uniqid + ']" class="form-control col-md-7 col-xs-12" required="required" onchange="show_multiple_imagetextbutton(this.value, '+max_items+', ' + uniqid + '); show_set_positioning(this.value, ' + uniqid + ');">';
-                            html += '<option value="" selected disabled>- Please Choose One -</option>';
-                        for (i = 1; i <= max_items; i++) {
-                            html += '<option value="'+i+'">'+i+'</option>';
-                        }
+                            for (i = 1; i <= max_items; i++) {
+                                html += '<option value="'+i+'">'+i+'</option>';
+                            }
                         html += "</select>";
                     html += '</div>';
                 html += '</div>';
 
                 // POSITIONING
-                html += '<div class="form-group" id="positioning-' + uniqid + '" style="display:none;">';
+                html += '<div class="form-group" id="positioning-' + uniqid + '">';
                     html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Positioning <span class="required">*</span></label>';
                     html += '<div class="col-md-6 col-sm-6 col-xs-12">';
                         html += '<select name="v_page_element_alignment[' + section_page_id + '][' + uniqid + ']" class="form-control col-md-7 col-xs-12">';
@@ -925,3 +932,187 @@ function show_set_positioning(this_value, identifier) {
     }
 }
 // IMAGE + TEXT + BUTTON - END
+
+// VIDEO - BEGIN
+function set_element_page_video(collapsed, uniqid, data) {
+    var element_type = "video";
+    var element_type_title = "Video";
+    var section_name = "";
+    var data_image = pagebuilder_no_img;
+    var is_required = 'required="required"';
+    var data_text = "";
+    var text_position_left = "";
+    if (data != "") {
+        section_name = data.section;
+        data_image = decodeURI(data.image);
+        is_required = "";
+        data_text = decodeURI(data.text);
+        if (data.text_position == "left") {
+            text_position_left = "selected";
+        }
+    }
+  
+    var html = '<div class="panel panel-content-element" id="pagebuilder_elm_' + uniqid + '">';
+        html += '<input type="hidden" name="v_page_element_type[' + section_page_id + '][' + uniqid + ']" value="'+element_type+'">';
+  
+    if (collapsed) {
+        html += '<a class="panel-heading" role="tab" data-toggle="collapse" data-parent="' + content_container + '" href="#collapse' + uniqid + '" aria-expanded="false">';
+    } else {
+        html += '<a class="panel-heading" role="tab" data-toggle="collapse" data-parent="' + content_container + '" href="#collapse' + uniqid + '" aria-expanded="true">';
+    }
+            html += '<h4 class="panel-title">'+element_type_title+' - Section <i id=section' + uniqid + ">" + section_name + '</i><span class="pull-right"><i class="fa fa-sort"></i><i class="fa fa-trash" style="color:red; margin-left: 20px;" onclick="delete_content_element(' + uniqid + ')"></i></span></h4>';
+        html += "</a>";
+  
+    if (collapsed) {
+        html += '<div id="collapse' + uniqid + '" class="panel-collapse collapse" role="tabpanel">';
+    } else {
+        html += '<div id="collapse' + uniqid + '" class="panel-collapse collapse in" role="tabpanel">';
+    }
+  
+            html += '<div class="panel-body">';
+
+                // SECTION
+                html += '<div class="form-group">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Section <span class="required">*</span></label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<input type="text" autocomplete="off" value="' + section_name + '" name="v_page_element_section[' + section_page_id + '][' + uniqid + ']" required="required" class="form-control col-md-7 col-xs-12" onblur="set_section_name(' + uniqid + ', this.value)">';
+                    html += '</div>';
+                html += '</div>';
+
+                // TYPE
+                html += '<div class="form-group">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Type <span class="required">*</span></label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<select name="v_page_element_video_type[' + section_page_id + '][' + uniqid + ']" class="form-control col-md-7 col-xs-12" required="required" onchange="show_input_video_text(this.value, ' + uniqid + ');">';
+                            html += '<option value="video">Video only</option>';
+                            html += '<option value="video + text">Video and Text</option>';
+                        html += "</select>";
+                    html += '</div>';
+                html += '</div>';
+
+                // VIDEO - TITLE
+                html += '<div class="form-group">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Video Title <span class="required">*</span></label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<input type="text" autocomplete="off" value="" name="v_page_element_video_title[' + section_page_id + '][' + uniqid + ']" required="required" class="form-control col-md-7 col-xs-12">';
+                    html += '</div>';
+                html += '</div>';
+
+                // VIDEO - LINK
+                html += '<div class="form-group">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Video Link <span class="required">*</span></label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<input type="text" autocomplete="off" value="" name="v_page_element_video[' + section_page_id + '][' + uniqid + ']" required="required" placeholder="https://www.youtube.com/watch?v=XXX" class="form-control col-md-7 col-xs-12">';
+                    html += '</div>';
+                html += '</div>';
+
+                // POSITIONING
+                html += '<div class="form-group input-video-text-' + uniqid + '" style="display:none;">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Positioning <span class="required">*</span></label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<select name="v_page_element_alignment[' + section_page_id + '][' + uniqid + ']" class="form-control col-md-7 col-xs-12">';
+                            html += '<option value="Left Video, Right Text">Left Video, Right Text</option>';
+                            html += '<option value="Left Text, Right Video">Left Text, Right Video</option>';
+                        html += "</select>";
+                    html += '</div>';
+                html += '</div>';
+
+                // TEXT
+                html += '<div class="form-group input-video-text-' + uniqid + '" style="display:none;">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Content Text</label>';
+                    html += '<div class="col-md-9 col-sm-9 col-xs-12">';
+                        html += '<textarea name="v_page_element_text[' + section_page_id + '][' + uniqid + ']" class="form-control col-md-7 col-xs-12 page-element-text-editor"></textarea>';
+                    html += '</div>';
+                html += '</div>';
+
+                // BUTTON - LINK TYPE
+                html += '<div class="form-group input-video-text-' + uniqid + '" style="display:none;">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Button - Link Type</label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<select name="v_page_element_button_link_type[' + section_page_id + '][' + uniqid + ']" id="button-link-type-' + uniqid + '" class="form-control col-md-7 col-xs-12" onchange="show_input_link(this.value, \'' + uniqid + '\')">';
+                            html += '<option value="no link">- NO LINK -</option>';
+                            html += '<option value="internal">Internal</option>';
+                            html += '<option value="external">External</option>';
+                        html += "</select>";
+                    html += '</div>';
+                html += '</div>';
+
+                // BUTTON - LINK (INTERNAL)
+                html += '<div class="form-group" id="pagebuilder-link-internal-' + uniqid + '" style="display:none;">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Button - Link (Internal)</label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<div class="input-group">';
+                            html += '<span class="input-group-addon">' + pagebuilder_url + '/</span>';
+                            html += '<input type="text" autocomplete="off" value="" name="v_page_element_button_link_internal[' + section_page_id + '][' + uniqid + ']" class="form-control col-md-7 col-xs-12">';
+                        html += '</div>';
+                    html += '</div>';
+                html += '</div>';
+
+                // BUTTON - LINK (EXTERNAL)
+                html += '<div class="form-group" id="pagebuilder-link-external-' + uniqid + '" style="display:none;">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Button - Link (External)</label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<input type="text" autocomplete="off" value="" name="v_page_element_button_link_external[' + section_page_id + '][' + uniqid + ']" class="form-control col-md-7 col-xs-12" placeholder="(sample: https://domain.com/promo-september)">';
+                    html += '</div>';
+                html += '</div>';
+
+                // BUTTON - LINK TARGET
+                html += '<div class="form-group pagebuilder-link-' + uniqid + '" style="display:none;">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Button - Link Target</label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<select name="v_page_element_button_link_target[' + section_page_id + '][' + uniqid + ']" class="form-control col-md-7 col-xs-12">';
+                            html += '<option value="">- Please Choose One -</option>';
+                            html += '<option value="on page">opened on the same page</option>';
+                            html += '<option value="new page">opened in a new page</option>';
+                        html += "</select>";
+                    html += '</div>';
+                html += '</div>';
+
+                // BUTTON - LABEL
+                html += '<div class="form-group pagebuilder-link-' + uniqid + '" style="display:none;">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Button - Label</label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<input type="text" autocomplete="off" value="" name="v_page_element_button_label[' + section_page_id + '][' + uniqid + ']" class="form-control col-md-7 col-xs-12" placeholder="optional">';
+                    html += '</div>';
+                html += '</div>';
+
+                // BUTTON - STYLE
+                html += '<div class="form-group pagebuilder-link-' + uniqid + '" style="display:none;">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Button - Style</label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<select name="v_page_element_button_style[' + section_page_id + '][' + uniqid + ']" class="form-control col-md-7 col-xs-12">';
+                            html += '<option value="">- Please Choose One -</option>';
+                            html += '<option value="style 1">Style 1</option>';
+                            html += '<option value="style 2">Style 2</option>';
+                        html += "</select>";
+                    html += '</div>';
+                html += '</div>';
+
+                // STATUS
+                html += '<hr><div class="form-group">';
+                    html += '<label class="control-label col-md-3 col-sm-3 col-xs-12">Status<span class="required">*</span></label>';
+                    html += '<div class="col-md-6 col-sm-6 col-xs-12">';
+                        html += '<select name="v_page_element_status[' + section_page_id + '][' + uniqid + ']" required="required" class="form-control col-md-7 col-xs-12">';
+                            html += '<option value="1">ACTIVE</option>';
+                            html += '<option value="0">Not Active</option>';
+                        html += '</select>';
+                    html += '</div>';
+                html += '</div>';
+            
+            html += '</div><!-- /.panel-body -->';
+        html += '</div><!-- /.panel-collapse -->';
+    html += '</div><!-- /.panel -->';
+  
+    return html;
+}
+
+function show_input_video_text(this_value, identifier) {
+    if (this_value == 'video + text') {
+        $('.input-video-text-'+identifier).show();
+    }else{
+        $('.input-video-text-'+identifier).hide();
+        $('#button-link-type-'+identifier).val('no link');
+        show_input_link('no link', identifier);
+    }
+}
+// VIDEO - END
